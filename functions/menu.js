a1777f1080246b69474c1253263a385457dff9bb
limoncito-21 <58611359+efrain88@users.noreply.github.com>
2021-12-10 10:10:45 -0500

    Add files via upload

---
+++ functions/menu.js
@@ -0,0 +1,126 @@
+exports.menu = (v) => {
+  return `┌─⊷ *PRINCIPAL* 
+▢ ${v}menuvc
+▢ ${v}bot <text>
+▢ ${v}info
+▢ ${v}owner
+▢ ${v}join <link>
+▢ ${v}listprem
+▢ ${v}listban
+▢ ${v}chatbot on/off
+▢ ${v}voicecmd on/off
+└─────────────
+≡ List Menu
+┌─⊷ *FUN* 
+▢ ${v}fake text|@user|text
+▢ ${v}preg
+└─────────────
+┌─⊷ *JUEGOS* 
+▢ ${v}verdad
+▢ ${v}reto
+▢ ${v}ppt
+▢ ${v}slot
+└─────────────
+┌─⊷ *STICKER* 
+▢ ${v}sticker <nombre>|<autor>
+▢ ${v}take <nombre>|<autor>
+▢ ${v}toimg
+▢ ${v}attp
+└─────────────
+┌─⊷ *IMAGENES* 
+▢ ${v}pinterest
+▢ ${v}imagen
+▢ ${v}girl
+▢ ${v}man
+▢ ${v}wallpaper
+└─────────────
+┌─⊷ *EDUCACIÓN* 
+▢ ${v}translate <lang><text>
+└─────────────
+┌─⊷ *BUSQUEDA*
+▢ ${v}playstore
+▢ ${v}ytsearch
+▢ ${v}google
+└─────────────
+┌─⊷ *DESCARGAS* 
+▢ ${v}play
+▢ ${v}play2
+▢ ${v}playvid
+▢ ${v}ytmp3 <link yt>
+▢ ${v}ytmp4 <link yt>
+▢ ${v}igdl <link>
+▢ ${v}tiktok <link>
+▢ ${v}tiktoknowm <link>
+▢ ${v}tiktokaudio <link>
+└─────────────
+┌─⊷ *CREADOR DE TEXTO*
+▢ ${v}nameninja <text>
+▢ ${v}typewriter <text>
+▢ ${v}blackpill <text>
+▢ ${v}sans <text>
+▢ ${v}castle <text>
+└─────────────
+┌─⊷ *ENCUESTAS* 
+▢ ${v}voting <text>
+▢ ${v}delvote
+▢ ${v}checkvote
+└─────────────
+┌─⊷ *ADMIN* 
+▢ ${v}setwelcome
+▢ ${v}seybye
+▢ ${v}delwelcome
+▢ ${v}delbye
+▢ ${v}simulate <welcome/bye>
+▢ ${v}group <open/close>
+▢ ${v}resetlink
+▢ ${v}banchat
+▢ ${v}unbanchat
+▢ ${v}hidetag
+▢ ${v}notify
+▢ ${v}tagall
+▢ ${v}setppgp
+▢ ${v}setname <text>
+▢ ${v}setdesc <text>
+▢ ${v}add <numero>
+▢ ${v}kick @user
+▢ ${v}promote @user
+▢ ${v}demote @user
+▢ ${v}warn @user
+▢ ${v}delwarn @user
+▢ ${v}warns @user
+└─────────────
+┌─⊷ *GRUPO*
+▢ ${v}welcome on/off
+▢ ${v}antidelete on/off
+▢ ${v}detect on/off
+▢ ${v}antilink on/off
+▢ ${v}link
+▢ ${v}infogp
+▢ ${v}profile
+▢ ${v}invite <51972xxxx> 
+└─────────────
+┌─⊷ *OWNER* 
+▢ ${v}addpremium @user
+▢ ${v}delpremium @user
+▢ ${v}ban @user
+▢ ${v}unban @user
+▢ ${v}view
+▢ ${v}grouplist
+▢ ${v}update
+▢ ${v}setppbot
+▢ ${v}setfakethumb
+▢ ${v}setprefix
+▢ >Efrain | Limoncito
+└─────────────`;
+};
+
+exports.menuVC = `
+┌─⊷ *MAIN* 
+▢ menu
+└─────────────
+┌─⊷ *BUSQUEDA*
+▢ google
+└─────────────
+┌─⊷ *DOWNLOAD* 
+▢ play
+└─────────────`;
